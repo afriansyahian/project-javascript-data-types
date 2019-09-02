1b6445a65ea193c8f96ef8d2d4f5daf61e9cf0ab
afriansyahian <afriansyah_i@rocketmail.com>
2019-09-02 16:08:38 +0700

    update

---
+++ index.js
@@ -0,0 +1,84 @@
+//JavaScript
+
+console.log ('hello World')
+
+
+console.log('hello World')
+
+const judul = 'Welcome to my web'
+
+console.log(`Hello ${judul}`)
+
+alert(`Hello ${judul}`)
+
+// NUMBER
+
+console.log(0)
+console.log(100)
+console.log(3.14)
+
+const age = 24
+
+console.log(`His age is ${age}`)
+
+alert(`His age is ${age}`)
+
+
+// OBJECT
+
+
+console. log({ name: 'Ian', age: 100, superPower: true})
+console.log({
+  name: 'Ian',
+  age: 100,
+  superPower: true
+})
+
+const hero = {name: 'Ian', age: 100, superPower: true}
+
+console.log(hero)
+console.log(hero['name'])
+console.log(hero.age)
+console.log(hero.superPower)
+
+alert(hero)
+alert(hero['name'])
+alert(hero.age)
+alert(hero.superPower)
+
+// ARRAY
+
+console.log([1,2,3])
+console.log([0,1,2,3])
+console.log(['Ian', 100, true])
+
+const person = ['Ian', 100, true]
+
+console.log(person)
+console.log(person[0])
+console.log(person[1])
+console.log(person[2])
+
+alert(person)
+alert(person [0])
+alert(person[1])
+alert(person[2])
+
+// BOOLEAN
+
+console.log(true)
+console.log(false)
+console.log(true, false)
+
+const toggle = single
+
+console.log(`i'm still ${toggle}`)
+
+alert(`i'm still ${toggle}`)
+
+
+
+
+
+
+
